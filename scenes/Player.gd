b7b71c4df67b9e127cbbc39532d8f3e6a25acb72
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-01-24 09:19:40 -0300

    Walking, destroying, collecting

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-var speed = 40  # speed in pixels/sec
+var speed = 50  # speed in pixels/sec
 var velocity = Vector2.ZERO
 var direction = 'left'
+var direction_vector = Vector2.LEFT
 onready var sprite: AnimatedSprite = get_node("AnimatedSprite")
 var firing = false
-var just_fired = false
+onready var raycast = get_node("RayCast2D")
 
 func get_input():
 	velocity = Vector2.ZERO
@@ -23,11 +24,15 @@ func get_input():
 		direction = 'up'
 	if Input.is_action_just_pressed('ui_select') and not firing:
 		firing = true
-		just_fired = true
 		sprite.play('fire-' + direction)
-		sprite.connect("animation_finished", self, 'finish_fire_animation')
+		if not sprite.is_connected("animation_finished", self, 'finish_fire_animation'):
+			sprite.connect("animation_finished", self, 'finish_fire_animation', [CONNECT_ONESHOT])
+		fire()
 	# Make sure diagonal movement isn't faster
 	velocity = velocity.normalized() * speed
+	if velocity != Vector2.ZERO:
+		direction_vector = velocity
+	
 	# Change animation
 	if not firing:
 		if velocity == Vector2.ZERO:
@@ -35,27 +40,41 @@ func get_input():
 		else:
 			sprite.play('walk-' + direction)
 	
-	
-func finish_fire_animation():
-	firing = false	
+
+func fire():
+	raycast.cast_to = direction_vector * 0.7
+	print('fire', raycast.cast_to)
+	if raycast.is_colliding():
+		print('is colliding')
+		if raycast.get_collider() is TileMap:
+			print('is tilemap')
+			var tilemap = raycast.get_collider()
+			var tile_pos = tilemap.world_to_map(position)
+			tile_pos -= raycast.get_collision_normal()
+			print('tile pos ', tile_pos)
+			var tile_id = tilemap.get_cellv(tile_pos)
+			if tile_id > 0:
+				var tile_name = tilemap.tile_set.tile_get_name(tile_id)
+				print('tilename', tile_name)
+				tilemap.set_cellv(tile_pos, 0)
+
+func finish_fire_animation(_x):
+	firing = false
+	sprite.play('idle-' + direction)
 
 func _physics_process(delta):
 	get_input()
-	var collision = move_and_collide(velocity * delta)
-
-	# Confirm the colliding body is a TileMap
-	if collision and collision.collider is TileMap:
-		var tile_pos = collision.collider.world_to_map(position)
-		tile_pos -= collision.normal
-		var tile_id = collision.collider.get_cellv(tile_pos)
-		var tile_name = collision.collider.tile_set.tile_get_name(tile_id)
-		print('collided with' + tile_name)
-		
-		if tile_name == 'item':
-			collision.collider.set_cellv(tile_pos, 0)
-		elif just_fired:
-			collision.collider.set_cellv(tile_pos, 0)
-		
-	
-	just_fired = false
+	velocity = move_and_slide(velocity)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		# Confirm the colliding body is a TileMap
+		if collision and collision.collider is TileMap:
+			var tile_pos = collision.collider.world_to_map(position)
+			tile_pos -= collision.normal
+			var tile_id = collision.collider.get_cellv(tile_pos)
+			if tile_id > 0:
+				var tile_name = collision.collider.tile_set.tile_get_name(tile_id)
+				#print('collided with' + tile_name)
+				if tile_name == 'item':
+					collision.collider.set_cellv(tile_pos, 0)
 
